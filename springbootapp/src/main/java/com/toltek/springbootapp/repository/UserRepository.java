ce927488200bf31639ec56fc9ece0615e64f2035
Toltek91 <113970778+Toltek91@users.noreply.github.com>
2024-05-15 18:18:57 +0700

    Spring boot PP_3_1_2

--- springbootapp/src/main/java/com/toltek/springbootapp/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.toltek.springbootapp.repository;
-
-import com.toltek.springbootapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
